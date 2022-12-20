0deb69b21cced72fadb49e0f96c30b8b18790d9f
mauros05 <emaurogtz@hotmail.es>
2022-12-20 09:39:21 -0600

    Add js to registro

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -9,8 +9,9 @@
         }
 
         public function Registro(){
-            $data["Title"]   = "Registro";
-            $data["Url_css"] = "assets/css/registration.css";
+            $data["Title"]      = "Registro";
+            $data["Url_css"]    = "assets/css/registration.css";
+            $data["Url_Jquery"] = "assets/js/Registro.js";
             require_once "views/Templates/Header.php";
             require_once "views/LoginViews/Registro.php";
             require_once "views/Templates/Footer.php";
