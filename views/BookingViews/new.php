35378fc4bf15b19de39b1966b532c64c2d9bae37
mauros05 <emaurogtz@hotmail.es>
2023-01-10 17:08:16 -0600

    Add icons and more style

--- views/BookingViews/new.php
+++ views/BookingViews/new.php
@@ -1,21 +1,73 @@
-<div class="container" >
+<div class="container">
+    <h1 class="mb-3">Reservar Apartamento</h1>
+    <div class="cards-container">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card text-black">
+                        <div class="card-body">
+                            <div class="text-center">
+                                <h5 class="card-title"><?= $res["nombre_ap"] ?></h5>
+                                <p class="text-muted mb-4"><?= $res["direccion"] ?></p>
+                            </div>
+                            <div>
+                                <div class="d-flex justify-content-between">
+                                    <span>Dueño</span><span><?= $res["nombre_us"] ?></span>
+                                </div>
+                                <div class="d-flex justify-content-between">
+                                    <span>Precio por noche</span><span>$<?= $res["precio"] ?></span>
+                                </div>
+                                <div class="d-flex justify-content-between">
+                                    <span>Numero de Noches</span><span>0</span>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-between total font-weight-bold mt-4">
+                                <span>Total</span><span>$0</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card text-black">
+                        
+                        <div class="card-body">
+                            <div class="text-center">
+                                <h5 class="card-title">Perfil de quien Reserva</h5>
+                                <p class="mb-4" style="color: blue;">Verifica tus datos</p>
+                            </div>
+                            <div>
+                                <div class="d-flex justify-content-between">
+                                    <span>Tu:</span><span><?= $res["nombre_us_booking"] ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <form action="" method="" enctype="multipart/form-data" class="mt-3" id="crear-apartamento">
-        <h1 class="mb-3">Reservar Apartamento</h1>
+        
 
         <div class="row mb-4">
             <div class="col">
                 <div>
-                    <label for="nombre" class="form-label">Fecha de Inicio:</label>
-                    <input type="date"  class="form-control" name="nombre" id="nombre" value=''/>
-                    <div id="nombre-error" style="color: red" hidden></div>
+                    <label for="start-date" class="form-label">Fecha de Inicio:</label>
+                    <input type="date"  class="form-control" name="start-date" id="start-date" value=''/>
+                    <div id="start-date-error" style="color: red" hidden></div>
                 </div>
             </div>
 
             <div class="col">
                 <div>
-                    <label for="direccion" class="form-label">Fecha de Finalizacion:</label>
-                    <input type="date" name="direccion" class="form-control" id="direccion" value=''/>
-                    <div id="direccion-error" style="color: red" hidden></div>
+                    <label for="end-date" class="form-label">Fecha de Finalizacion:</label>
+                    <input type="date" name="end-date" class="form-control" id="end-date" value=''/>
+                    <div id="end-date-error" style="color: red" hidden></div>
                 </div>
             </div>
         </div>
@@ -24,57 +76,8 @@
         <input type="text" name="id_user_booking" value="<?= $_SESSION["id_usuario"] ?>" hidden>
         <input type="text" name="accion" value="cb" hidden>
             
-        <button type="button" class="btn btn-dark mt-3 mb-3" id="crearApartamento">Crear</button>
+        <button type="button" class="btn btn-dark mt-3 mb-3" id="crearApartamento">Reservar</button>
     </form>
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6 col-xl-4">
-                <div class="card text-black">
-                    
-                    <div class="card-body">
-                        <div class="text-center">
-                            <h5 class="card-title"><?= $res["nombre_ap"] ?></h5>
-                            <p class="text-muted mb-4"><?= $res["direccion"] ?></p>
-                        </div>
-                        <div>
-                            <div class="d-flex justify-content-between">
-                                <span>Dueño</span><span><?= $res["nombre_us"] ?></span>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <span>Precio por noche</span><span>$<?= $res["precio"] ?></span>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <span>Numero de Noches</span><span>0</span>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-between total font-weight-bold mt-4">
-                            <span>Total</span><span>$0</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8 col-lg-6 col-xl-4">
-                <div class="card text-black">
-                    
-                    <div class="card-body">
-                        <div class="text-center">
-                            <h5 class="card-title">Perfil de quien Reserva</h5>
-                            <p class="mb-4" style="color: blue;">Verifica tus datos</p>
-                        </div>
-                        <div>
-                            <div class="d-flex justify-content-between">
-                                <span>Tu:</span><span><?= $res["nombre_us_booking"] ?></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 
@@ -90,3 +93,10 @@
     </div>
   </div>
 </div>
+
+<style>
+.cards-container{
+    display: flex;
+}
+
+</style>
--- views/Templates/Navbar.php
+++ views/Templates/Navbar.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <?php if (isset($_SESSION["id_usuario"])) { ?>
-      <a class="navbar-brand"  href="http://localhost/arbnb-clone/index.php?ac=h">Arbnb</a>
+      <a class="navbar-brand"  href="http://localhost/arbnb-clone/index.php?ac=h">Arbnb<i class="fa-solid fa-plane ms-2" style="height: 30px; width: 30px; color: white;"></i></a>
     <?php } else { ?>
-      <a class="navbar-brand"  href="http://localhost/arbnb-clone/">Arbnb</a>
+      <a class="navbar-brand"  href="http://localhost/arbnb-clone/">Arbnb<i class="fa-solid fa-plane ms-2" style="height: 30px; width: 30px; color: white;"></i></a>
     <?php } ?>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -21,8 +21,12 @@
           </li>
         <?php } ?>
       </ul>
-      <form class="d-flex" role="search">
+      <form class="d-flex align-items-center" role="search">
         <?php if (isset($_SESSION["id_usuario"])) { ?>
+          <div class="d-flex align-items-center me-5 mt-2">
+            <i class="fa-solid fa-user " style="height: 30px; width: 30px; color: white;"></i>
+            <p class="" style="color: white;"><?= $_SESSION["username"] ?></p> 
+          </div>
           <a href="logout.php" class="btn btn-outline-danger">LogOut</a>
         <?php } else { ?>
           <a href="login.php?ac=lg" class="btn btn-outline-success">Login</a>
